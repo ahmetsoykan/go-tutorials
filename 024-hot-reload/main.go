67f516b634db16e2035d872ce258f0ca0c8a48c6
Ahmet Soykan <soykanselcuk1@gmail.com>
2020-06-06 17:17:45 +0300

    edit main.go

--- 024-hot-reload/main.go
+++ 024-hot-reload/main.go
@@ -1,21 +1,51 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
-	"io/ioutil"
+	"io"
+	"os"
 	"strings"
 	"time"
 )
 
+type Config map[string]string
+
+func ReadConfig(filename string) Config {
+
+	config := Config{}
+
+	file, _ := os.Open(filename)
+
+	defer file.Close()
+
+	reader := bufio.NewReader(file)
+
+	for {
+		line, err := reader.ReadString('\n')
+
+		if equal := strings.Index(line, "="); equal >= 0 {
+			if key := strings.TrimSpace(line[:equal]); len(key) > 0 {
+				value := ""
+				if len(line) > equal {
+					value = strings.TrimSpace(line[equal+1:])
+				}
+				// assign the config map
+				config[key] = value
+			}
+		}
+		if err == io.EOF {
+			break
+		}
+	}
+	return config
+}
+
 func main() {
 	for {
-		file, _ := ioutil.ReadFile("./config")
-		keys := strings.Split(string(file), "=")
+		conf := ReadConfig(`./config`)
+		fmt.Println(conf["NAME"])
 
-		for i := 0; i < len(keys); i++ {
-			fmt.Println(keys[i])
-		}
-
-		time.Sleep(1 * time.Second)
+		time.Sleep(1 * time.Minute)
 	}
 }
